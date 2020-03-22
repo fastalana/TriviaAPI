b3023f7723b6747bd62b8ad242a040bc91f74a5f
Alana Bellucci <fastalana@gmail.com>
2020-03-22 09:46:01 -0600

    Cleaning up comments.

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -13,25 +13,17 @@ def create_app(test_config=None):
   app = Flask(__name__)
   setup_db(app)
   
-  '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
+  # Set up CORS. Allow '*' for origins. 
   CORS(app)
 
-  '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
+  # @app.after_request decorator sets Access-Control-Allow for Methods and Headers
   @app.after_request
   def after_request(response):
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
-
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests 
-  for all available categories.
-  '''
+ 
+  # Create an endpoint to handle GET requests for all available categories.
   @app.route('/categories')
   def retrieve_categories():
     categories = list(map(Category.format, Category.query.all()))
