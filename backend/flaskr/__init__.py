ee0a84d05ef6233656aa548731cbc9c2b08829ef
Alana Bellucci <fastalana@gmail.com>
2020-03-23 11:37:09 -0600

    Created an endpoint to DELETE question using a question ID.

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -73,64 +73,74 @@ def create_app(test_config=None):
   # Clicking on the page numbers should update the questions. 
 
 
-  '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
 
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
+  # @TODO: 
+  # Create an endpoint to DELETE question using a question ID. 
+  @app.route('/questions/<int:question_id>', methods=['DELETE'])
+  def delete_question(question_id):
+    try:
+      question = Question.query.filter(Question.id == question_id).one_or_none()
 
-  '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
+      if question is None:
+        abort(404)
 
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
+      question.delete()
 
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
+      selection = Question.query.order_by(Question.id).all()
+      current_questions = paginate_questions(request, selection)
+      
+      return jsonify({
+        'success': True,
+        # 'questions': current_questions,
+        'deleted': question_id,
+        'total_questions': len(Question.query.all())
+      })
 
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
-  '''
+    except:
+      abort(422)
 
-  '''
-  @TODO: 
-  Create a GET endpoint to get questions based on category. 
+  # TEST: When you click the trash icon next to a question, the question will be removed.
+  # This removal will persist in the database and when you refresh the page. 
+ 
+  # @TODO: 
+  # Create an endpoint to POST a new question, 
+  # which will require the question and answer text, 
+  # category, and difficulty score.
 
-  TEST: In the "List" tab / main screen, clicking on one of the 
-  categories in the left column will cause only questions of that 
-  category to be shown. 
-  '''
+  # TEST: When you submit a question on the "Add" tab, 
+  # the form will clear and the question will appear at the end of the last page
+  # of the questions list in the "List" tab.  
 
 
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
 
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-  '''
+  # @TODO: 
+  # Create a POST endpoint to get questions based on a search term. 
+  # It should return any questions for whom the search term 
+  # is a substring of the question. 
 
-  '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
+  # TEST: Search by any phrase. The questions list will update to include 
+  # only question that include that string within their question. 
+  # Try using the word "title" to start. 
+
+  # @TODO: 
+  # Create a GET endpoint to get questions based on category. 
+
+  # TEST: In the "List" tab / main screen, clicking on one of the 
+  # categories in the left column will cause only questions of that 
+  # category to be shown. 
+
+  # @TODO: 
+  # Create a POST endpoint to get questions to play the quiz. 
+  # This endpoint should take category and previous question parameters 
+  # and return a random questions within the given category,
+  # if provided, and that is not one of the previous questions. 
+
+  # TEST: In the "Play" tab, after a user selects "All" or a category,
+  # one question at a time is displayed, the user is allowed to answer
+  # and shown whether they were correct or not. 
+
+  # @TODO: 
+  # Create error handlers for all expected errors 
+  # including 404 and 422. 
   
-  return app
-
-    
+  return app 
