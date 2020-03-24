8ef9448eb44003b5be1353721144759d4ba6a320
Alana Bellucci <fastalana@gmail.com>
2020-03-24 08:36:19 -0600

    Setting up unit tests and testing the first three.

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -29,11 +29,47 @@ class TriviaTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+    # TODO
+    # Write at least one test for each test for successful operation and for expected errors.
+    def test_get_categories(self):
+        response = self.client().get('/categories')
+        data = json.loads(response.data)
 
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['category'])
+        self.assertEqual(data['total_categories'], 6)
+
+    def test_get_paginated_questions(self):
+        response = self.client().get('/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['question'])
+        self.assertEqual(len(data['question']), 10)
+        self.assertEqual(data['total_questions'], 19)
+
+    def test_error_for_invalid_questions_page(self):
+        response = self.client().get('/questions?page=100000')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+    # def test_question_delete(self):
+    # def test_error_question_delete_id_does_not_exist(self):
+    # def test_error_question_delete_invalid_id(self):
+    # def test_create_question(self):
+    # def test_error_create_question_with_missing_data(self):
+    # def test_search_question(self):
+    # def test_error_search_question_with_no_search(self):
+    # def test_get_questions_by_catgeory(self):
+    # def test_error_get_questions_by_category_id_does_not_exist(self):
+    # def test_error_get_questions_by_category_invalid_id(self):
+    # def test_quiz_question(self):
+    # def test_error_quiz_question_with_no_data(self):
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
